863e0e632fba9223ea9b660aca7ede85b09ed0da
georgebzhang <georgebzhang5@gmail.com>
2019-03-01 13:33:34 -0800

    two sum

---
+++ Cpp_LeetCode/main.cpp
@@ -0,0 +1,8 @@
+#include "two_sum.h"
+
+int main() {
+	Two_Sum s;
+	s.test();
+
+	std::cin.get();
+}
---
+++ Cpp_LeetCode/two_sum.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Two_Sum {
+public:
+	std::vector<int> twoSum(std::vector<int>& nums, int target) {
+		std::vector<int> result;
+		std::unordered_map<int, int> map;
+		for (int i = 0; i < nums.size(); ++i) {
+			if (map.find(nums[i]) != map.end()) {
+				result.push_back(map[nums[i]]);
+				result.push_back(i);
+			}
+			else
+				map[target - nums[i]] = i;
+		}
+		return result;
+	}
+
+	void print_answer(std::vector<int> ans) {
+		for (int i = 0; i < ans.size(); ++i) {
+			std::cout << ans[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	void test() {
+		std::vector<int> nums{ 2, 7, 11, 15 };
+		int target = 9;
+		std::vector<int> ans = twoSum(nums, target);
+		print_answer(ans);
+
+	}
+};
